b553963b7e49adaf5626ab56721bb341475391ab
Chimoha31 <mokochii1108@gmail.com>
2022-09-26 15:24:09 -0700

    SinglePost

--- 03blog/src/App.js
+++ 03blog/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './pages/home/Home'
-import TopBar from './topbar/TopBar'
+import TopBar from './components/topbar/TopBar'
+import Single from './pages/single/Single'
+import Write from './pages/write/Write'
 
 const App = () => {
   return (
     <>
       <TopBar />
-      <Home />
+      <Write />
     </>
   )
 }
