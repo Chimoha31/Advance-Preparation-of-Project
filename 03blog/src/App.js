c1e7cb484226b15b597774719f208263c3f0a108
Chimoha31 <mokochii1108@gmail.com>
2022-09-25 17:18:56 -0700

    init

---
+++ 03blog/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './pages/home/Home'
+import TopBar from './topbar/TopBar'
+
+const App = () => {
+  return (
+    <>
+      <TopBar />
+      <Home />
+    </>
+  )
+}
+
+export default App
