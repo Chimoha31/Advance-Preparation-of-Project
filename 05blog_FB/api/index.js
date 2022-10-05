bcec5f16a08dcecc3a3cef3738314041389d6947
Chimoha31 <mokochii1108@gmail.com>
2022-10-04 21:32:32 -0700

    auth

--- 05blog_FB/api/index.js
+++ 05blog_FB/api/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 const dotenv = require("dotenv");
 dotenv.config();
+
+app.use(express.json());
+
 
 // Connect mongoDB
 const mongoURL = process.env.MONGO_URL
@@ -15,6 +19,9 @@ mongoose.connect(mongoURL, (err) => {
     console.log("mongoDB is successfully connected");
   }
 });
+
+// Router
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
   console.log("Server is running 👯‍♀️");
