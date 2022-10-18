e08dbf681a892c7ba94f09caad77cbef2cf24193
Chimoha31 <mokochii1108@gmail.com>
2022-10-17 19:54:55 -0700

    Installed multer

--- 05blog_FB/api/index.js
+++ 05blog_FB/api/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
-const categoryRoute = require('./routes/categories');
+const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 
 const dotenv = require("dotenv");
@@ -14,7 +14,7 @@ dotenv.config();
 app.use(express.json());
 
 // Connect mongoDB
-const mongoURL = process.env.MONGO_URL
+const mongoURL = process.env.MONGO_URL;
 mongoose.connect(mongoURL, (err) => {
   if (err) {
     console.log("There is an error on mongoDB");
@@ -23,11 +23,25 @@ mongoose.connect(mongoURL, (err) => {
   }
 });
 
+// multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
 // Router
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
-app.use('/api/categories', categoryRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen(PORT, () => {
   console.log("Server is running 👯‍♀️");
