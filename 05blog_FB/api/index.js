f8808f38c8b23153b45d593c7a6adb2f5fbf4170
Chimoha31 <mokochii1108@gmail.com>
2022-10-17 19:41:06 -0700

    Done category

--- 05blog_FB/api/index.js
+++ 05blog_FB/api/index.js
@@ -5,12 +5,13 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require('./routes/categories');
+const multer = require("multer");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
 app.use(express.json());
-
 
 // Connect mongoDB
 const mongoURL = process.env.MONGO_URL
@@ -26,6 +27,7 @@ mongoose.connect(mongoURL, (err) => {
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use('/api/categories', categoryRoute);
 
 app.listen(PORT, () => {
   console.log("Server is running 👯‍♀️");
